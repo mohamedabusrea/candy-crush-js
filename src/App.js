e0f76ff2077510d3fb2c63a13c1d63ccb13337d5
mohamedabusrea <mohamedabusrea@gmail.com>
2020-02-02 15:32:31 +0100

    - Project Initialization - Draw the grid with random colors has been finished - Find the matched blocks and turn them into white

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import {random as _random, times as _times, differenceWith as _differenceWith, isEqual as _isEqual} from 'lodash';
+
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    createBlocksMatrix();
+  }, []);
+
+  const [gridBlocksArray, setGridBlocksArray] = useState([]);
+  const gridData = {rowsLength: 10, columnsLength: 10, columnWidth: 50};
+  const colorsArray = ['red', 'green', 'blue', 'yellow'];
+  const blockObject = (rowIndex, columnIndex) => {
+    return {
+      rowIndex,
+      columnIndex,
+      color: colorsArray[_random(colorsArray.length - 1)],
+    };
+  };
+
+  let matchedBlocksArray = [];
+  const createBlocksMatrix = () => {
+    const grid = _times(gridData.rowsLength, rowIndex => {
+      return _times(gridData.columnsLength, columnIndex => blockObject(rowIndex, columnIndex));
+    });
+
+    setGridBlocksArray(grid);
+  };
+  const checkTheSiblings = (blockData, isCleanArray) => {
+    isCleanArray && (matchedBlocksArray = []); //Clean the array with every new click
+    matchedBlocksArray.push(blockData);
+
+    const result = getBlockSiblings(blockData); //Get the current block's matched siblings
+
+    const uniqueBlocksArray = _differenceWith(result, matchedBlocksArray, _isEqual); //remove the duplicates that already been added to the array
+
+    if (uniqueBlocksArray.length) {
+      uniqueBlocksArray.map(block => checkTheSiblings(block));
+    }
+    else {
+      (matchedBlocksArray.length > 1) && removeMatchedBlocks();
+    }
+  };
+  const getBlockSiblings = ({rowIndex, columnIndex}) => {
+    const blockSiblingsArray = [];
+    const currentBlockColor = gridBlocksArray[rowIndex][columnIndex].color;
+    const leftBlockColor = (columnIndex - 1 >= 0) && gridBlocksArray[rowIndex][columnIndex - 1].color;
+    const rightBlockColor = (columnIndex + 1 <= gridData.columnsLength - 1) && gridBlocksArray[rowIndex][columnIndex + 1].color;
+    const topBlockColor = rowIndex - 1 >= 0 && gridBlocksArray[rowIndex - 1][columnIndex].color;
+    const bottomBlockColor = (rowIndex + 1 <= gridData.rowsLength - 1) && gridBlocksArray[rowIndex + 1][columnIndex].color;
+
+    if (currentBlockColor === leftBlockColor) {
+      blockSiblingsArray.push({rowIndex, columnIndex: columnIndex - 1});
+    }
+    if (currentBlockColor === rightBlockColor) {
+      blockSiblingsArray.push({rowIndex, columnIndex: columnIndex + 1});
+    }
+    if (currentBlockColor === topBlockColor) {
+      blockSiblingsArray.push({rowIndex: rowIndex - 1, columnIndex});
+    }
+    if (currentBlockColor === bottomBlockColor) {
+      blockSiblingsArray.push({rowIndex: rowIndex + 1, columnIndex});
+    }
+
+    return blockSiblingsArray;
+  };
+  const removeMatchedBlocks = () => {
+    const tmp = [...gridBlocksArray];
+
+    matchedBlocksArray.map(blockData => tmp[blockData.rowIndex][blockData.columnIndex].color = 'white');
+
+    setGridBlocksArray(tmp);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className='GameContainer'>
+          <div className='GameContainer__title'>
+            Candy Crush Game
+          </div>
+          <div className="GameContainer__body"
+               style={{
+                 width: gridData.columnsLength * gridData.columnWidth,
+                 height: gridData.columnsLength * gridData.columnWidth,
+               }}>
+            {gridBlocksArray.map((rowArray, rowIndex) =>
+                                     rowArray.map((blockData, columnIndex) =>
+                                                      <div key={(rowIndex * gridData.rowsLength) + columnIndex}
+                                                           className="GameContainer__block"
+                                                           style={{
+                                                             top: `${blockData.rowIndex * gridData.columnWidth}px`,
+                                                             left: `${blockData.columnIndex * gridData.columnWidth}px`,
+                                                             backgroundColor: blockData.color,
+                                                           }}
+                                                           onClick={() => checkTheSiblings({
+                                                                                             rowIndex: blockData.rowIndex,
+                                                                                             columnIndex: blockData.columnIndex,
+                                                                                           }, true)}>
+                                                        {(rowIndex * gridData.rowsLength) + columnIndex}
+                                                      </div>,
+                                     ),
+            )}
+          </div>
+        </div>
+      </div>
   );
 }
 
